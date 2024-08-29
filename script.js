40d784e9acc058e19100f7021bd1d11b51d17dd6
Soumya Ranjan Mishra <118365110+smranjanmishra@users.noreply.github.com>
2024-08-29 08:19:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
-    //Write your code here
-
-
+    // Select all <strong> elements
+    const boldWords = document.getElementsByTagName('strong');
+    
+    // Change the color of each <strong> element to green
+    for (let word of boldWords) {
+        word.style.color = 'green';
+    }
 }
 
-
 function return_normal() {
-    //Write your code here
-
+    // Select all <strong> elements
+    const boldWords = document.getElementsByTagName('strong');
     
+    // Change the color of each <strong> element back to black
+    for (let word of boldWords) {
+        word.style.color = 'black';
+    }
 }
